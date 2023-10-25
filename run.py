3400b02fa676793ac46fd045c41eab5ec5856bf0
osf912 <123300938+osf912@users.noreply.github.com>
2023-10-25 21:50:28 +0200

    Add sketch for class TryRow

--- run.py
+++ run.py
@@ -19,22 +19,51 @@ def __init__(self, code):
 class CodeRow(Row):
     """
     A CodeRow is the row with the code to find in the
-    mastermind game. One code is a number from 0 to num.
+    mastermind game. One code is a number from 0 to 5.
     """
 
     def __init__(self):
         """
-        num is the range of the code, number between 0 and num
+        Fill self.code_part with random number from 0 to 5.
         """
         rand_list = []
         for i in range(4):
             rand_list.append(randint(0,5))
-        print(rand_list)
         Row.__init__(rand_list)
         self.response_part = [2,2,2,2]
     
-    def printCodeRow(self):
+    def print_CodeRow(self):
         print("Code:   | X | X | X | X |")
     
+class TryRow(Row):
+    """
+    A TryRow is the try to find the code in the game
+    """
+    def __init__(self, try_num):
+        super().__init__(self, [0, 0, 0, 0])
+        self.try_num = try_num # Try Number for print-method
+
+    def getTry():
+        """
+        Ask the new try from the user
+        """
+        try = input("Enter try: (4 comma separated Numbers (0-5)")
+        check_try(try)
+
+        return try
+
+    def check_try(try).
+
+    def get_response(self, response_part):
+        self.response_part = response_part
+
+    def print_TryRow():
+        c = self.code_part
+        r = self.response_part
+        print(f"{self.try_num}      | {c[0]} | {c[1]} | {c[2]} | {c[3]}  ||  {r[0]} | {r[1]} | {r[2]} | {r[3]}")
+
 code = CodeRow()
-code.printCodeRow()
+code.print_CodeRow()
+
+try = TryRow()
+try.getTr
