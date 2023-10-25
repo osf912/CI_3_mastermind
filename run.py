e3c4cfa4319af433f2653e193f4506e85b854806
osf912 <123300938+osf912@users.noreply.github.com>
2023-10-25 21:24:10 +0200

    Initial commit, add class Row and CodeRow

--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+class Row:
+    """
+    A Row is a line in the game with a codepart ( a list of
+    4 numbers (0-5) (6 colors in the origin game) and a response
+    part, where the responses are 
+        2 -> there is one code at the right position
+        1 -> there is a correct code in the list, but not the right position
+        0 -> no response
+    """
+    code_part = [0,0,0,0]
+    response_part = [0,0,0,0]
+
+def __init__(self, code):
+    self.code_part = code
+    self.response_part = [0,0,0,0]
+
+class CodeRow(Row):
+    """
+    A CodeRow is the row with the code to find in the
+    mastermind game. One code is a number from 0 to num.
+    """
+
+    def __init__(self):
+        """
+        num is the range of the code, number between 0 and num
+        """
+        rand_list = []
+        for i in range(4):
+            rand_list.append(randint(0,5))
+        print(rand_list)
+        Row.__init__(rand_list)
+        self.response_part = [2,2,2,2]
+    
+    def printCodeRow(self):
+        print("Code:   | X | X | X | X |")
+    
+code = CodeRow()
+code.printCodeRow()
